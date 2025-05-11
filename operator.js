e8d89e75a71a8a501744d594c25de0b52fdbb352
tasrif-66 <mtasrif20@gmail.com>
2025-05-11 18:49:17 +0600

    operator

---
+++ operator.js
@@ -0,0 +1,28 @@
+const income = 100;
+const expence = 40;
+const result = income - expence;
+console.log(result);
+
+const pencel = 10;
+const useTime = 2;
+const koiberUseKorbo = pencel / useTime;
+console.log(koiberUseKorbo);
+
+const iHave = 60;
+const evrytimeTimekhoroch = 15;
+const koiberkhoroch = iHave / evrytimeTimekhoroch;
+const baki = iHave % evrytimeTimekhoroch;
+console.log(koiberUseKorbo, baki);
+
+const string1 = "Hello";
+const string2 = "World";
+const result1 = string1 + string2;
+console.log(result1);
+
+const buyRice = 153;
+const buyDalil = 261;
+console.log(buyRice + buyDalil);
+
+
+
+
